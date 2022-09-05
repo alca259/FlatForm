e7ffe34712d489b2ced10c77fcd1b91216a9d63b
Alca259 <alca259@gmail.com>
2022-09-05 19:52:42 +0200

    mmmmmmmmmmmmmmmmmm

---
+++ Alca259.Forms/UserControls/NavigationMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Alca259.Forms.UserControls
+{
+    public partial class NavigationMenu : UserControl
+    {
+        public NavigationMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
